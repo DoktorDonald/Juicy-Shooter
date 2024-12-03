b8c18b8bbbf3c3b71e53754b9b76c78d01690e28
DoktorDonald <poan_greenwally@outlook.com>
2024-12-03 16:23:29 +0100

    Accuracy Decrease + Bug
    Added a decrease in accuracy after continuous firing, also found a bug when firing while holding the cursor over the player.

--- Köttig Shooter/Assets/Scripts/Gun.cs
+++ Köttig Shooter/Assets/Scripts/Gun.cs
@@ -8,47 +8,96 @@ public class Gun : MonoBehaviour
     [SerializeField] float firerate;
     [SerializeField] float bulletLifeTime;
 
+    [SerializeField] float spreadIncrease;
+    [SerializeField] float spreadDelay;
+    [SerializeField] float spreadDecrease;
+    [SerializeField] float maxSpread;
+    [SerializeField] float overheatTime;
+    [SerializeField] float rechargeTime;
+
     [SerializeField] float bulletOriginOffset;
 
+    [SerializeField] float spread;
 
     Vector2 mousePos;
+    Vector2 gunPos;
+
     Vector3 aimPoint;
     float aimAngle;
+
     float shootingTimer;
+    float spreadTimer;
+
+    bool canShoot = true;
 
     private void Update()
     {
         HandleAiming();
         HandleShooting();
+        HandleSpread();
 
         shootingTimer += Time.deltaTime;
+
+        gunPos = new Vector2(transform.position.x, transform.position.y);
     }
 
     private void HandleAiming()
     {
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        aimPoint = mousePos;
 
-        if (aimPoint.x - transform.position.x > 0)
+        if (mousePos.x - transform.position.x > 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
-            aimAngle = Mathf.Atan((aimPoint.y - transform.position.y) / (aimPoint.x - transform.position.x)) * 180 / Mathf.PI;
+            aimAngle = Mathf.Atan((mousePos.y - transform.position.y) / (mousePos.x - transform.position.x)) * 180 / Mathf.PI;
         }
         else
         {
             transform.localScale = new Vector3(-1, 1, 1);
-            aimAngle = Mathf.Atan((aimPoint.y - transform.position.y) / (aimPoint.x - transform.position.x)) * 180 / Mathf.PI;
+            aimAngle = Mathf.Atan((mousePos.y - transform.position.y) / (mousePos.x - transform.position.x)) * 180 / Mathf.PI;
         }
 
         transform.eulerAngles = new Vector3(0, 0, aimAngle);
     }
 
+    void HandleSpread()
+    {
+        if (Input.GetMouseButton(0) && canShoot)
+        {
+            spreadTimer += Time.deltaTime;
+
+            if (spreadTimer >= spreadDelay)
+            {
+                spread += spreadIncrease * Time.deltaTime;
+                spread = Mathf.Clamp(spread, 0, maxSpread);
+            }
+
+            if (spreadTimer >= overheatTime)
+            {
+                canShoot = false;
+                Invoke(nameof(EnableShooting), rechargeTime);
+            }
+        }
+        else if (spread > 0)
+        {
+            spread -= spreadDecrease * Time.deltaTime;
+        }
+        else if (spread <= 0)
+        {
+            spreadTimer = 0;
+        }
+
+        Vector2 rand = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        aimPoint = mousePos + (rand * spread * (mousePos - gunPos).magnitude);
+    }
+
     private void HandleShooting()
     {
-        if (Input.GetMouseButton(0) && shootingTimer > 1/firerate)
+        if (Input.GetMouseButton(0) && canShoot && shootingTimer > 1/firerate)
         {
-            GameObject bullet = Instantiate(bulletPrefab, new Vector2(transform.position.x, transform.position.y) + (mousePos - new Vector2(transform.position.x, transform.position.y)).normalized * bulletOriginOffset, Quaternion.identity);
-            bullet.GetComponent<Rigidbody2D>().linearVelocity = (mousePos - new Vector2(transform.position.x, transform.position.y)).normalized * bulletSpeed;
+            GameObject bullet = Instantiate(bulletPrefab, gunPos + (mousePos - gunPos).normalized * bulletOriginOffset, Quaternion.identity);
+
+            Vector3 velocity = ((Vector2)aimPoint - (gunPos + (mousePos - gunPos).normalized * bulletOriginOffset)).normalized * bulletSpeed;
+            bullet.GetComponent<Rigidbody2D>().linearVelocity = velocity;
            
             bullet.GetComponent<Bullet>().StartTimer(bulletLifeTime);
 
@@ -56,10 +105,18 @@ public class Gun : MonoBehaviour
         }
     }
 
+    void EnableShooting()
+    {
+        canShoot = true;
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.cyan;
         Gizmos.DrawWireSphere(mousePos, 0.2f);
-        Gizmos.DrawWireSphere(new Vector2 (transform.position.x, transform.position.y) + (mousePos - new Vector2(transform.position.x, transform.position.y)).normalized * bulletOriginOffset, 0.2f);
+        Gizmos.DrawWireSphere(gunPos + (mousePos - gunPos).normalized * bulletOriginOffset, 0.2f);
+
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(aimPoint, 0.1f);
     }
 }
