81b740d44fbb7f74842ac57251cba616fdebc583
Kevin <kevin.nygard@elev.ga.lbs.se>
2025-01-23 11:49:59 +0100

    Enemy yippie (Den funkar nästan)

--- Köttig Shooter/Assets/Scripts/Enemy.cs
+++ Köttig Shooter/Assets/Scripts/Enemy.cs
@@ -9,21 +9,44 @@ public class Enemy : MonoBehaviour
     [SerializeField] bool canShoot = false;
 
     Vector2 enemyMovement;
+    LayerMask layerMask;
 
     CapsuleCollider2D capsuleCollider;
     Rigidbody2D myRigidbody;
     SpriteRenderer spriteRenderer;
+
+    bool facingRight = true;
 
     private void Start()
     {
         capsuleCollider = GetComponent<CapsuleCollider2D>();
         myRigidbody = GetComponent<Rigidbody2D>();
 
+        layerMask = LayerMask.GetMask("Ground");
         hitPoints = maxHitPoints;
+        enemyMovement = new Vector2(moveSpeed, 0f);
     }
-    private void Update()
+    private void FixedUpdate()
     {
-        
+        myRigidbody.linearVelocityX = enemyMovement.x;
+
+        Debug.DrawLine(transform.position, transform.position + transform.TransformDirection(new Vector3(transform.position.x + 5f, transform.position.y - 5f, 0f)) * Mathf.Sqrt(50f));
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(new Vector3(transform.position.x + 5f, transform.position.y - 5f, 0f)), Mathf.Sqrt(50f), layerMask) == false)
+        {
+            if (facingRight)
+            {
+                gameObject.transform.localScale = new Vector3(-1, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+
+                facingRight = false;
+            }
+            else if (!facingRight)
+            {
+                gameObject.transform.localScale = new Vector3(1, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+
+                facingRight = true;
+            }
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
